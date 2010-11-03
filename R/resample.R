ba6fec386499d70136eb24444254503d6d276c52
Henrik Bengtsson <hb@aroma-project.org>
2010-11-03 18:29:34 +0000

    Version: 1.5.6 [2010-11-03] o Added resample(), which contrary to sample() also works   when drawing from a single element.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@420 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/resample.R
@@ -0,0 +1,51 @@
+###########################################################################/**
+# @RdocDefault resample
+#
+# @title "Sample values from a set of elements"
+#
+# \description{
+#  @get "title".
+#  Contrary to @see "base::sample", this function also works as
+#  expected when there is only one element in the set to be sampled, cf. [1].
+#  This function originates from the example code of @see "base::sample"
+#  as of R v2.12.0.
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{x}{A @vector of any length and data type.}
+#   \item{...}{Additional arguments passed to @see "base::sample.int".}
+# }
+#
+# \value{
+#  Returns a sampled @vector of the same data types as argument \code{x}.
+# }
+#
+# @author
+#
+# \seealso{
+#   Internally @see "base::sample" is used.
+# }
+#
+# \references{
+#   [1] Henrik Bengtsson, 
+#       \emph{Using sample() to sample one value from a single value?},
+#       R-devel mailing list, 2010-11-03.\cr
+# }
+#
+# @keyword IO
+# @keyword programming
+#*/########################################################################### 
+setMethodS3("resample", "default", function(x, ...) {
+  x[sample.int(length(x), ...)];
+})
+
+
+###########################################################################
+# HISTORY: 
+# 2010-11-03
+# o Created from example("sample") in R v2.12.0.  See R-devel thread
+#   'Using sample() to sample one value from a single value?' by
+#   Henrik Bengtsson on 2010-11-03.
+###########################################################################
