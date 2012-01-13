136906e1b21d29c618a571763bc6bb2a73747b00
Henrik Bengtsson <hb@aroma-project.org>
2012-01-13 03:30:13 +0000

    Version: 1.9.10 [2012-01-12] o CLEANUP: reassignInPackage() calls function that are considered   "unsafe" by the new CRAN policies, i.e. unlockBinding() and   assignInNamespace().  However, we still wish to keep this method   available to advanced users.  In order to avoid getting NOTEs   from R CMD check, we have "hidden" those unsafe function calls.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@691 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/reassignInPackage.R
+++ R/reassignInPackage.R
@@ -62,8 +62,10 @@ setMethodS3("reassignInPackage", "default", function(name, pkgName, value, keepO
   if (keepOld)
     attr(value, "oldValue") <- oldValue;
 
-  unlockBinding(name, env);
-  assignInNamespace(name, value, ns=pkgName, envir=env);
+  unlockBindingT <- base::unlockBinding;
+  unlockBindingT(name, env);
+  assignInNamespaceT <- utils::assignInNamespace;
+  assignInNamespaceT(name, value, ns=pkgName, envir=env);
   assign(name, value, envir=env);
   lockBinding(name, env);
 
@@ -72,6 +74,12 @@ setMethodS3("reassignInPackage", "default", function(name, pkgName, value, keepO
 
 ############################################################################
 # HISTORY:
+# 2012-01-12
+# o CLEANUP: reassignInPackage() calls function that are considered
+#   "unsafe" by the new CRAN policies, i.e. unlockBinding() and
+#   assignInNamespace().  However, we still wish to keep this method
+#   available to advanced users.  In order to avoid getting NOTEs
+#   from R CMD check, we have "hidden" those unsafe function calls.
 # 2007-07-13
 # o Now reassignInPackage() can reassign non-exported values by using
 #   getAnywhere() instead of get().
