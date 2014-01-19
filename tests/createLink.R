cbb4edb0f5cb7dedcaae50f996322c39d50c94a3
Henrik Bengtsson <hb@aroma-project.org>
2014-01-19 08:49:11 +0000

    Version: 1.29.3 [2014-01-18] o ROBUSTNESS: Added package system tests for createLink().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@1977 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tests/createLink.R
@@ -0,0 +1,23 @@
+library("R.utils")
+verbose <- Verbose(threshold=-1)
+
+pathname <- file.path(tempdir(), "foo.txt")
+pathname <- getAbsolutePath(pathname)
+cat(file=pathname, "Hello world!\n")
+
+methods <- c("unix-symlink", "windows-ntfs-symlink", "windows-shortcut")
+for (method in methods) {
+  verbose && enterf(verbose, "Method '%s'", method)
+  verbose && cat(verbose, "Target: ", pathname)
+  link <- sprintf("%s-to-%s", method, basename(pathname))
+  verbose && cat(verbose, "Link to create: ", link)
+  tryCatch({
+    linkR <- createLink(link=link, target=pathname, method=method)
+    verbose && cat(verbose, "Link returned: ", linkR)
+  }, error = function(ex) {
+    verbose && cat(verbose, "Failed to create link: ", ex$message)
+  })
+  verbose && exit(verbose)
+}
+
+if (isFile(pathname)) file.remove(pathname)
