89e0af1320463a06daf4e9d6673ffa54d4c83de4
Henrik Bengtsson <hb@aroma-project.org>
2014-02-24 20:37:09 +0000

    Version: 1.29.9 [2014-02-24] o ROBUSTNESS: Added a package redundancy test for a bug occurring in   R.oo (< 1.18.0) causing R to core dump (with "Error: SET_VECTOR_ELT()   can only be applied to a 'list', not a 'integer'") or gives an error   (with "Error: not a weak reference") under certain conditions when a   registered finalizer tried to reload R.oo if it was unloaded.  This   occurred only on R prior to R 3.0.2 patched (2014-02-21 r65057).  Also,   the 'methods' package needs to be attached, so it is still not clear   what is the true cause of the bug.  In R.oo (>= 1.18.0) this bug is   avoided.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@2036 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/finalizer_crash.R
+++ tests/finalizer_crash.R
@@ -1,4 +1,8 @@
-# This script crashes in ~20s on R 3.0.2 with R.oo (< 1.18.0)
+# This script crashes in ~40s on R 2.15.3 patched and R 3.0.2 with
+# R.oo (< 1.18.0).  It only does so when 'methods' is attached.  It
+# does not crash on R 3.0.2 patched (2014-02-21 r65057) and beyond.
+# /HB 2014-02-22
+library("methods")
 library("R.methodsS3")
 loadNamespace("R.utils")
 
