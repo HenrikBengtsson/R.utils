d044b7fa6f414fb3faa4918d3b6124ce721df45c
Henrik Bengtsson <hb@aroma-project.org>
2014-02-24 21:00:04 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@2038 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/finalizer_crash.R
+++ tests/finalizer_crash.R
@@ -1,7 +1,8 @@
 # This script crashes in ~40s on R 2.15.3 patched and R 3.0.2 with
 # R.oo (< 1.18.0).  It only does so when 'methods' is attached.  It
 # does not crash on R 3.0.2 patched (2014-02-21 r65057) and beyond.
-# It appears to not always occur with 'R CMD check'. /HB 2014-02-22
+# It appears to not crash with 'R CMD check' on 2.15.3 but if run
+# via 'Rscript' or similar. /HB 2014-02-22
 library("methods")
 library("R.methodsS3")
 loadNamespace("R.utils")
