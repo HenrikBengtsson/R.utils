de0fde3fd92c36f4e53b12e95f624615261e4eaf
Henrik Bengtsson <hb@aroma-project.org>
2014-04-26 22:47:15 +0000

    Version: 1.31.0 [2014-04-26] o Added assignment operator %<-% for delayed assignments.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@2095 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/OperatorDelayedAssign.R
@@ -0,0 +1,46 @@
+###########################################################################/**
+# @RdocFunction "%<-%"
+# @alias "%<-%"
+#
+# @title "Assignment operator for delayed assignments"
+#
+# \description{
+#  @get "title" such that the expression on the right-hand side (RHS)
+#  will not be evaluated until the value of the assigned variable
+#  (on left-hand side (LHS)) is evaluated.
+# }
+#
+# \usage{
+#   x \%<-\% value
+# }
+#
+# \arguments{
+#   \item{x}{The variable name (possibly quoted).}
+#   \item{value}{The value to be assigned to \code{x}.}
+# }
+#
+# \value{
+#   Returns (invisibly) @NULL.
+#   Note, contrary to @see "base::<-", it is \emph{not}
+#   meaniful to do \code{a \%<-\% b \%<-\% c \%<-\% 6}.
+# }
+#
+# @author
+#
+# \seealso{
+#   Internally @see "base::delayedAssign" is used.
+# }
+#
+# @keyword internal
+#*/###########################################################################
+`%<-%` <- function(x, value) {
+  name <- as.character(substitute(x));
+  delayedAssign(name, value=value, assign.env=parent.frame());
+} # `%<-%`()
+
+
+##############################################################################
+# HISTORY:
+# 2014-04-26
+# o Created. Extracted from private RSP templates.
+##############################################################################
