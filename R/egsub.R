b1dc73a3435b1e68cb31fb315a06ab102d605a28
Henrik Bengtsson <hb@aroma-project.org>
2014-05-08 04:56:51 +0000

    o Added egsub(), which is gsub() for expressions with some bells and whistles.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.utils@2167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/egsub.R
@@ -0,0 +1,71 @@
+###########################################################################/**
+# @RdocFunction egsub
+#
+# @title "Global substitute of expression using regular expressions"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{pattern}{A @character string with the regular expression to be
+#      matched, cf. @see "base::gsub".}
+#   \item{replacement}{A @character string of the replacement to use
+#      when there is a match, cf. @see "base::gsub".}
+#   \item{x}{The @expression to be modified.}
+#   \item{...}{Additional arguments passed to @see "base::gsub"}
+#   \item{value}{If @TRUE, the value of the replacement itself is used
+#      to look up a variable with that name and then using that variables
+#      value as the replacement.  Otherwise the replacement value is used.}
+#   \item{envir, inherits}{An @environment from where to find the variable
+#      and whether the search should also include enclosing frames, cf.
+#      @see "base::get".  Only use if \code{value} is @TRUE.}
+# }
+#
+# \value{
+#   Returns an @expression.
+# }
+#
+# @examples "../incl/egsub.Rex"
+#
+# @author
+#
+# @keyword utilities
+# @keyword programming
+#*/###########################################################################
+egsub <- function(pattern, replacement, expr, ..., value=TRUE, envir=parent.frame()) {
+  # Substitute?
+  if (is.symbol(expr)) {
+    code <- as.character(expr);
+    if (regexpr(pattern, code, ...) != -1L) {
+      name <- sub(pattern, replacement, code, ...);
+
+      # Substitute with the *value* of a variable, or a variable?
+      if (value) {
+        expr <- get(name, envir=envir);
+      } else {
+        expr <- as.symbol(name);
+      }
+    }
+    return(expr)
+  }
+
+  # Iterate?
+  if (is.language(expr)) {
+    for (ii in seq_along(expr)) {
+      expr[[ii]] <- egsub(pattern, replacement, expr[[ii]], ..., value=value);
+    }
+  }
+
+  expr
+} # egsub()
+
+
+##############################################################################
+# HISTORY:
+# 2014-05-06
+# o Added egsub(), which is gsub() for expression.
+# o Created.
+##############################################################################
