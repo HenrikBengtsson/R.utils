08e7a2294eb9915b484df5bf29686c4f395f6906
hb <hb@aroma-project.org>
2014-09-18 18:57:47 -0700

    Wrong version of the test file

--- tests/writeDataFrame.R
+++ tests/writeDataFrame.R
@@ -15,7 +15,7 @@ pathname <- tempfile(fileext=".tsv")
 writeDataFrame(data, file=pathname)
 
 # Append another set of rows
-writeDataFrame(data, file=pathname, append=TRUE, header=NULL, col.names=FALSE)
+writeDataFrame(data, file=pathname, append=TRUE)
 
 # There should only be one header and one set of column names
 print(readLines(pathname))
