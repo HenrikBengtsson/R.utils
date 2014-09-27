c0a8d7856a0887e95a5b57cd1e76da7f22806880
hb <hb@aroma-project.org>
2014-09-27 14:06:06 -0700

    Added package tests for mkdirs()

---
+++ tests/mkdirs.R
@@ -0,0 +1,23 @@
+library("R.utils")
+
+pathT <- tempdir()
+mprint(pathT)
+
+path <- file.path(pathT, "foo")
+mprint(path)
+mkdirs(path)
+stopifnot(isDirectory(path))
+
+# Windows has a undocumented "feature" that for some set
+# ups on some machines (not all) it will for instance
+# silently drop a trailing period and create the directory
+# without it, e.g. 'G.S.' becomes 'G.S', cf.help("dir.create").
+# See also https://bugs.r-project.org/bugzilla3/show_bug.cgi?id=15996
+path <- file.path(pathT, "G.S.")
+mprint(path)
+mkdirs(path)
+tryCatch({
+  stopifnot(isDirectory(path))
+}, error = function(ex) {
+  mprint(ex)
+})
