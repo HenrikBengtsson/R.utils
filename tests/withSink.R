3067d6f98cbc800b8612528e70dd3a950cd07b45
hb <hb@aroma-project.org>
2015-01-12 22:21:59 -0800

    Better reporting of warnings in withSink.R test

--- tests/withSink.R
+++ tests/withSink.R
@@ -7,10 +7,22 @@ mfile <- function(file, ...) {
   mcat("-------------------------------------------------\n")
 } # mfile()
 
+clearWarnings <- function() {
+  assign("last.warning", value=NULL, envir=baseenv())
+}
+
+
+# Display warnings as they occur
+oopts <- options(warn=1L)
+
+# Clear all warnings
+clearWarnings()
+
 cons0 <- showConnections()
 
 # Divert standard output
 pathname <- tempfile(fileext=".output.txt")
+mprint(pathname)
 res <- withSink(file=pathname, {
   print(letters)
   NULL
@@ -21,6 +33,7 @@ mprint(warnings())
 
 # Divert standard error/messages
 pathname <- tempfile(fileext=".message.txt")
+mprint(pathname)
 res <- withSink(file=pathname, type="message", {
   mprint(letters)
   NULL
@@ -31,6 +44,7 @@ mprint(warnings())
 
 # Divert standard output (and make sure to close any other sinks opened)
 pathname <- tempfile(fileext=".output2.txt")
+mprint(pathname)
 res <- withSink(file=pathname, {
   print(letters)
   pathnameT <- tempfile(fileext=".output3.txt")
@@ -40,9 +54,15 @@ res <- withSink(file=pathname, {
 }, append=TRUE)
 mfile(pathname)
 mprint(warnings())
+clearWarnings()
+
 
 # Assert that all connections opened were closed
 cons1 <- showConnections()
 mprint(cons0)
 mprint(cons1)
 stopifnot(all.equal(cons1, cons0))
+
+# Reset how warnings are displayed and clear any generated
+options(oopts)
+clearWarnings()
