0455b811a7b76a2193b49a4a886936002ed3f4fb
hb <hb@aroma-project.org>
2015-02-04 14:12:15 -0800

    Argument 'recursive' of findFiles() can now also be numeric. Just like for listDirectory(), argument 'recursive' of findFiles() can in addition to be FALSE (depth=0) and TRUE (depth=+Inf), be any non-negative numeric number specifying how deeply the recursiv search should be done.

--- tests/listDirectory.R
+++ tests/listDirectory.R
@@ -4,7 +4,11 @@ path <- system.file(package="R.utils")
 print(listDirectory(path))
 print(listDirectory(path, pattern="DESCRIPTION"))
 print(listDirectory(path, recursive=TRUE))
+print(listDirectory(path, recursive=+Inf))
+print(listDirectory(path, recursive=FALSE))
+print(listDirectory(path, recursive=0L))
 print(listDirectory(path, recursive=1L))
+print(listDirectory(path, recursive=2L))
 
 # Non-existing
 print(listDirectory("unknown-path"))
