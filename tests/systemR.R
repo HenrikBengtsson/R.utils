f5f4ae50d85bb8b6956ac08f959270960e77ceef
hb <hb@aroma-project.org>
2015-05-27 17:28:23 -0700

    Fixed test not working on Unix

--- tests/systemR.R
+++ tests/systemR.R
@@ -1,4 +1,9 @@
 library("R.utils")
 
-res <- systemR("--slave -e cat(runif(1))", intern=TRUE, verbose=TRUE)
+message("*** systemR() ...")
+
+res <- systemR('--slave -e "cat(runif(1))"', intern=TRUE, verbose=TRUE)
 cat("A random number: ", res, "\n", sep="")
+
+message("*** systemR() ... DONE")
+
