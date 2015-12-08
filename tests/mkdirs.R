d4110dfab22949d6c62a23af3a1e6b13481f1af1
hb <hb@aroma-project.org>
2015-12-08 11:51:54 -0800

    TESTS: Add tests for mkdirs(..., mustWork=TRUE) [#22]

--- tests/mkdirs.R
+++ tests/mkdirs.R
@@ -1,7 +1,12 @@
 library("R.utils")
 
+message("*** mkdirs() ...")
+
+message("*** mkdirs(..., recursive=TRUE) ...")
+
 pathT <- tempdir()
 mprint(pathT)
+stopifnot(isDirectory(pathT))
 
 path <- file.path(pathT, "foo", "bar")
 mprint(path)
@@ -11,14 +16,50 @@ stopifnot(isDirectory(path))
 paths <- c(dirname(path), path)
 stopifnot(all(isDirectory(paths)))
 
-removeDirectory(dirname(path), recursive=TRUE)
+path <- dirname(path)
+removeDirectory(path, recursive=TRUE)
+stopifnot(!isDirectory(path))
+
+message("*** mkdirs(..., recursive=TRUE) ... DONE")
 
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# EXCEPTIONS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+message("*** mkdirs(..., mustWork=TRUE) ...")
+
+path <- file.path(pathT, "foo")
+res <- mkdirs(path)
+stopifnot(isDirectory(path))
+
+pathname <- file.path(path, "bar")
+cat("Hello", file=pathname)
+stopifnot(isFile(pathname))
+
+res <- mkdirs(pathname)
+stopifnot(isFile(pathname), !isDirectory(pathname))
+
+res <- try(mkdirs(pathname, mustWork=TRUE), silent=TRUE)
+stopifnot(inherits(res, "try-error"))
+stopifnot(isFile(pathname), !isDirectory(pathname))
+
+
+removeDirectory(path, recursive=TRUE)
+stopifnot(!isDirectory(path))
+
+message("*** mkdirs(..., mustWork=TRUE) ... DONE")
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# SPECIAL:
 # Windows has a undocumented "feature" that for some set
 # ups on some machines (not all) it will for instance
 # silently drop a trailing period and create the directory
 # without it, e.g. 'G.S.' becomes 'G.S', cf.help("dir.create").
 # See also https://bugs.r-project.org/bugzilla3/show_bug.cgi?id=15996
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+message("*** mkdirs('G.S.') ...")
+
 path <- file.path(pathT, "G.S.")
 mprint(path)
 mkdirs(path)
@@ -28,3 +69,7 @@ tryCatch({
 }, error = function(ex) {
   mprint(ex)
 })
+
+message("*** mkdirs('G.S.') ... DONE")
+
+message("*** mkdirs() ... DONE")
