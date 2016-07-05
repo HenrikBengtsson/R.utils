4e2780991bb27e408248da73bfb04e29fa0cf958
hb <hb@aroma-project.org>
2016-07-05 16:24:19 -0700

    Add nullfile() [#51]

---
+++ R/nullfile.R
@@ -0,0 +1,29 @@
+#########################################################################/**
+# @RdocFunction nullfile
+#
+# @title "Gets the pathname of the NULL device on the current platform"
+#
+# \description{
+#  @get "title".  On Windows, it returns \code{"NIL"}.  On all other
+#  platforms include Unix and macOS it returns \code{"/dev/null"}.
+# }
+#
+# \usage{
+#   @usage nullfile
+# }
+#
+# \value{
+#   Returns a @character string.
+# }
+#
+# @author
+#
+# @keyword "programming"
+# @keyword "file"
+#*/#########################################################################
+nullfile <- function() {
+  switch(.Platform$OS.type,
+    windows="NUL",
+    "/dev/null"
+  )
+}
