0a41fab395275dde8dfa6e879e8805d5f0f486b1
Henrik Bengtsson <hb@aroma-project.org>
2019-05-31 17:02:27 -0700

    tweak

--- R/createLink.R
+++ R/createLink.R
@@ -88,11 +88,11 @@ setMethodS3("createLink", "default", function(link=".", target, skip=!overwrite,
   if (any(file.exists(links))) {
     if (skip) {
       pathnameL <- Arguments$getReadablePathname(link, mustExist=TRUE)
-      equal <- identical(pathnameLA, target)
+      equal <- identical(pathnameL, target)
       # Be more forgiving on Windows system, i.e. assume a
       # case-insensitive file system
       if (!equal && (.Platform$OS.type == "windows")) {
-        equal <- identical(tolower(pathnameLA), tolower(target))
+        equal <- identical(tolower(pathnameL), tolower(target))
       }
 
       if (!equal) {
